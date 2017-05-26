08ee424a645e8315919fd155b7006ef4c98705d9
ValShatravenko <valik.kiev@gmail.com>
2017-05-26 23:52:24 +0300

    First two tasks

---
+++ Assets/LevelController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+	
+	public static LevelController current;
+	
+	void Awake() {
+		current = this;
+	}
+}
